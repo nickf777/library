f6f6f9a750bca2eb21203731aaa0c73b427bad0b
Nick Faller <nickfallerguitar@gmail.com>
2019-12-16 12:17:29 -0600

    Setup ofHTML, CSS, JS and some project planning

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # library
+
+This is the library project from the Odin Project. The purpose of this project is to learn more about:
+- JavaScript Prototypal Inheritance
+- Firebase and localStorage
+- More DOM Manipulation
+- General OOP Fundamentals
+- Creating an appealing design using fundamental CSS concepts
---
+++ script.js
@@ -0,0 +1,7 @@
+// I want an array that contains a collection of object literals of the book class
+// To do the above I need a book constructor that takes in author, title, number of pages, read status, and any other info
+// I need a function that creates a new book object using Object.create and adds it to the array
+// I need a render function that iterates through the array of books and renders them on the page with their own card/information panel
+// I need to program a change read status button that changes the read status with a click
+// I need to learn and figure out the fundamentals of firebase to make it possible to store the information.
+// I need to clean it all up and make it look visually appealing
